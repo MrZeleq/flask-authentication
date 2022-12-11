353ba456417901d5835e57ca1871a38399ab677f
MrZeleq <87999031+MrZeleq@users.noreply.github.com>
2022-12-11 15:14:39 +0100

    Update #2
    Dodano wyświetlanie wszystkich recordów z tabeli User po zalogowaniu użytkownika

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+from .models import User
 
 views = Blueprint('views', __name__)
 
@@ -11,5 +12,7 @@ def mainPage():
 @views.route('/home')
 @login_required
 def home():
-    return render_template("home.html", user=current_user)
+    users = User.query.all()
+    return render_template("home.html", user=current_user, users=users)
+
 
